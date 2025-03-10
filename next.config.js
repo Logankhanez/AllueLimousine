8083fb36e2abb03c64ad9823204884a34bac72ff
Logankhanez <82732434+Logankhanez@users.noreply.github.com>
2025-03-10 08:59:13 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  module.exports = nextConfig
-  
-  
+  // La ligne "output: export" a été supprimée
+  i18n: {
+    locales: ['fr'],
+    defaultLocale: 'fr',
+  },
+  images: {
+    domains: ['images.unsplash.com', 'plus.unsplash.com'],
+  },
+};
+
+module.exports = nextConfig;
